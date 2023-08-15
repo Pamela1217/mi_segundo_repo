ae553cb5286acc618f52835aa8f8becdd07d8369
Sandra López Torres <sandra.lopeztores@gmail.com>
2023-08-15 17:12:23 -0600

    Trabajo concluido

--- apppracticaprogramada3.R
+++ apppracticaprogramada3.R
@@ -5,48 +5,55 @@ ui <- fluidPage(
   titlePanel(h4(strong("Planificador de eventos"))),
   sidebarLayout(
     sidebarPanel(widht=6,
-                 
-                 textInput(inputId = "nombre_evento", 
-                           label = "Nombre del evento",
-                           placeholder = "Boda de María y Julio"
+        textInput(inputId = "nombre_evento", 
+                  label = "Nombre del evento",
+                  placeholder = "Boda de María y Julio"
                  ),
-                 tags$hr(style="border-color:orange;"),
-                 
-                 selectInput(inputId = "selector_provincia", # el id de este "input", debe ser único
-                             label = "Seleccione la provincia en la que vive:", 
-                             choices = c("San José", "Alajuela","Cartago", "Heredia","Guanacaste","Puntarenas","Limón"),
-                             multiple = FALSE
-                 ),
-                 tags$hr(style="border-color:orange;"),
-                 
-                 numericInput(inputId = "numero_edad", 
-                              label = "Ingrese su edad", 
-                              value = 18, #indica el valor inicial a mostrar
-                              min = 15, #permite indicar el valor mínimo
-                              max = 105, #permite indicar el valor máximo
-                              step = 1, #permite indicar el incremento entre números
-                 ),
-                 tags$hr(style="border-color:orange;")
-                 
-    ),
-    mainPanel(widht=6,
-              textOutput("Informacion_Personal"),
-              br(),
-              br(),
-              div(img(src = "Imagensesion12.png", height=140,widht=600), aling="center")
-    ),
-    
-  ),
+        
+        selectInput(inputId = "tema_evento", # el id de este "input", debe ser único
+                    label = "Tema del evento", 
+                    choices = c("Fiesta de cumpleaños","Boda","Conferencia", "Otro"),
+                    multiple = FALSE
+        ),
+        
+        numericInput(inputId = "numero_invitados", 
+                     label = "Número de invitados", 
+                     value = 58, #indica el valor inicial a mostrar
+                     min = 25, #permite indicar el valor mínimo
+                     max = 120, #permite indicar el valor máximo
+                     step = 1, #permite indicar el incremento entre números
+        ),
+        
+        selectInput(inputId = "selector_provincia", # el id de este "input", debe ser único
+                    label = "Provincia", 
+                    choices = c("San José", "Alajuela","Cartago", "Heredia","Guanacaste","Puntarenas","Limón"),
+                    multiple = FALSE
+        ),
+         
   
-)
+                ),
+    
+        
+    mainPanel(
+      h2("Vamos a planificar su día especial",style="color:blue",align="center"),
+      tags$hr(style="border-color:blue;"),
+      h4("Detalles del evento:"),
+      textOutput("detalles_evento1"),
+      hr(),
+      textOutput("detalles_evento2"),
+      hr(),
+      textOutput("detalles_evento3")
+      
+    )
+                 
+    )
+  )
+
 
 server <- function(input, output, session) {
-  
-  output$Informacion_Personal <- renderText(paste0("Le damos la bienvenida ",input$nombre_persona," !
-                                                   Usted ha seleccionado ",input$selector_provincia,
-                                                   " como su ubicación en Costa Rica y su edad es de ",
-                                                   input$numero_edad," años." ))
-  
+  output$detalles_evento1 <- renderText(paste0("Nombre del evento: ",input$nombre_evento))
+  output$detalles_evento2 <- renderText(paste0("Tema del evento: ",input$tema_evento))
+  output$detalles_evento3 <- renderText(paste0("Número de invitados y ubicación: El evento ",input$tema_evento," tendrá ",input$numero_invitados, " y será llevada a cabo en ",input$selector_provincia))
 }
 
 shinyApp(ui, server)
